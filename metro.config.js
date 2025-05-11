f3377fc3df3bb77a32f6a728b54a5cb09a1173b9
Mohammad Zoraiz <zoraizmohammad@gmail.com>
2025-05-11 14:31:15 -0400

    trying hella more debug, still now working

--- metro.config.js
+++ metro.config.js
@@ -3,4 +3,13 @@ const { withNativeWind } = require('nativewind/metro');
   
 const config = getDefaultConfig(__dirname);
   
+// Add any custom config here
+config.resolver.extraNodeModules = {
+  stream: require.resolve('readable-stream'),
+  crypto: require.resolve('react-native-crypto'),
+  buffer: require.resolve('buffer/'),
+  util: require.resolve('util/'),
+  process: require.resolve('process/browser'),
+};
+
 module.exports = withNativeWind(config, { input: './global.css' });
